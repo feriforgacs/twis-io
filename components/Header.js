5cc7bc8dfe24b1a0330ecd355820b4ca82d9d6b8
Ferenc Forgacs <forgacsf@gmail.com>
2021-05-23 18:21:27 +0200

    🚧 header and hero section

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,19 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Header() {
 	return (
 		<header id="header">
+			<div className="logo">
+				<Link href="/">
+					<a>
+						<Image src="/twis-logo.png" alt="twis logo" width={50} height={50} />
+						<span className="font--mulish">twis</span>
+					</a>
+				</Link>
+			</div>
 			<nav>
 				<ul>
-					<li>
-						<Link href="/">
-							<a>Home</a>
-						</Link>
-					</li>
-
 					<li>
 						<Link href="/features">
 							<a>Features</a>
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,10 @@ import Layout from "../components/Layout";
 export default function Home() {
 	return (
 		<Layout>
-			<h1>twis</h1>
+			<div id="hero">
+				<h1 className="font--mulish">Grow your database with story-like experiences</h1>
+				<h2>Twis helps you create experiences that are more in line with the story format so you can grow your database in a fun and more engaging way.</h2>
+			</div>
 		</Layout>
 	);
 }
