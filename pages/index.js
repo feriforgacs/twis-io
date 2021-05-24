1dca8fc0f4a0a781732aa86bcfd0f7614167d5f1
Ferenc Forgacs <forgacsf@gmail.com>
2021-05-24 21:09:14 +0200

    [wip] mockuo

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,13 @@ export default function Home() {
 			<div id="hero">
 				<h1 className="font--mulish">Grow your database with story-like experiences</h1>
 				<h2>Twis helps you create experiences that are more in line with the story format so you can grow your database in a fun and more engaging way.</h2>
+
+				<a href="https://app.twis.io" className="btn btn--primary btn--large">
+					Try for free
+				</a>
 			</div>
+
+			<div id="mockup"></div>
 		</Layout>
 	);
 }
