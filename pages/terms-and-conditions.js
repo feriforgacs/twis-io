d99572d021c79cca4d66b09b9a3ed3d899cb28d3
Ferenc Forgacs <forgacsf@gmail.com>
2021-06-01 18:17:28 +0200

    terms and conditions

---
+++ pages/terms-and-conditions.js
@@ -0,0 +1,116 @@
+import Layout from "../components/Layout";
+
+export default function TermsAndConditionsPage() {
+	return (
+		<Layout className="subpage">
+			<div className="container">
+				<section className="content">
+					<h1>Terms and Conditions</h1>
+					<p>
+						<em>Last updated: June 1, 2021</em>
+					</p>
+
+					<p>Please read these Terms and Conditions ("Terms", "Terms and Conditions") carefully before using the https://twis.io, https://app.twis.io, https://go.twis.io websites (the "Service") operated by Twis.io ("us", "we", or "our").</p>
+
+					<p>Your access to and use of the Service is conditioned on your acceptance of and compliance with these Terms. These Terms apply to all visitors, users and others who access or use the Service.</p>
+
+					<p>By accessing or using the Service you agree to be bound by these Terms. If you disagree with any part of the terms then you may not access the Service.</p>
+
+					<h3>Links To Other Web Sites</h3>
+
+					<p>Our Service may contain links to third-party web sites or services that are not owned or controlled by Twis.io.</p>
+
+					<p>Twis.io has no control over, and assumes no responsibility for, the content, privacy policies, or practices of any third party web sites or services. You further acknowledge and agree that Twis.io shall not be responsible or liable, directly or indirectly, for any damage or loss caused or alleged to be caused by or in connection with use of or reliance on any such content, goods or services available on or through any such web sites or services.</p>
+
+					<p>We strongly advise you to read the terms and conditions and privacy policies of any third-party web sites or services that you visit.</p>
+
+					<h3>Use License</h3>
+
+					<p>Permission is granted to temporarily download one copy of the materials (information or software) on Twis.io's website for personal, non-commercial transitory viewing only. This is the grant of a license, not a transfer of title, and under this license you may not:</p>
+
+					<ul>
+						<li>modify or copy the materials;</li>
+						<li>use the materials for any commercial purpose, or for any public display (commercial or non-commercial);</li>
+						<li>attempt to decompile or reverse engineer any software contained on Twis.io's website;</li>
+						<li>remove any copyright or other proprietary notations from the materials; or</li>
+						<li>transfer the materials to another person or "mirror" the materials on any other server.</li>
+					</ul>
+
+					<p>This license shall automatically terminate if you violate any of these restrictions and may be terminated by Twis.io at any time. Upon terminating your viewing of these materials or upon the termination of this license, you must destroy any downloaded materials in your possession whether in electronic or printed format.</p>
+
+					<h3>Disabling the service, withdrawal and refund</h3>
+
+					<p>
+						Users may discontinue the Service at any time{" "}
+						<a href="https://app.twis.io/account" target="_blank" rel="noopener noreferrer" className="link">
+							under their account
+						</a>{" "}
+						or by contacting us directly.
+					</p>
+
+					<p>When disabled, Users are not allowed to claim for payments already made even if these relate to unused portions of the Service purchased.</p>
+
+					<p>The Owner reserves the right, however, in its sole discretion and without involving charges or obligation to Users generally, to grant reimbursement in exceptional cases after evaluating the specific circumstances of the case.</p>
+
+					<p>In accordance with the right of withdrawal, the user may, within 7 days from the date of activation or payment of the Service, make a refund request, by sending notice to us via email (refund@twis.io) and including any information necessary to identify the purchase to which the claim relates.</p>
+
+					<p>Refunds will be made within 30 days after receipt of the request using the same payment method involved during the original purchase.</p>
+
+					<p>For more information or for assistance regarding a refund request, the User is asked to contact us via email.</p>
+
+					<h3>The Service is provided "as is"</h3>
+
+					<p>The Service is provided by Twis.io "as is", with no express or implied warranty for accuracy or availability.</p>
+
+					<h3>Service interruption</h3>
+
+					<p>Twis.io reserves the right to add and remove functionalities or features as well as suspend or even discontinue the Service, either temporarily or permanently. In case of final discontinuation, Twis.io will do the utmost to allow Users to withdraw their information held by us.</p>
+
+					<h3>Service reselling</h3>
+
+					<p>Users are not allowed to reproduce, duplicate, copy, sell, resell or exploit any portion of Twis.io and its Service without express prior written permission, granted either directly or through a proper reselling program.</p>
+
+					<h3>Forbidden use</h3>
+
+					<p>The Service shall be used only in accordance with these Terms.</p>
+
+					<p>Users may not:</p>
+					<ul>
+						<li>reverse engineer, decompile, disassemble, modify or create derivative works based on Twis.io or any portion of it;</li>
+						<li>circumvent any technology used by Twis.io or its licensors to protect content accessible via it;</li>
+						<li>use any robot, spider, site search/retrieval application, or other automated device, process or means to access, retrieve, scrape, or index any portion of Twis.io or its content;</li>
+						<li>rent, lease or sublicense Twis.io;</li>
+						<li>defame, abuse, harass, use threatening practices, threaten or violate the legal rights of others in any other way (such as rights of privacy and publicity);</li>
+						<li>disseminate or publish content that is unlawful, obscene, illegitimate, defamatory or inappropriate;</li>
+					</ul>
+
+					<h3>Age eligibility</h3>
+
+					<p>Users declare themselves to be adult according to their applicable legislation. Under no circumstance may persons under the age of 13 use Twis.io.</p>
+
+					<h3>Disclaimer</h3>
+
+					<p>The materials on our website are provided on an 'as is' basis. Twis.io makes no warranties, expressed or implied, and hereby disclaims and negates all other warranties including, without limitation, implied warranties or conditions of merchantability, fitness for a particular purpose, or non-infringement of intellectual property or other violation of rights.</p>
+
+					<p>Further, Twis.io does not warrant or make any representations concerning the accuracy, likely results, or reliability of the use of the materials on its website or otherwise relating to such materials or on any sites linked to this site.</p>
+
+					<h3>Governing Law</h3>
+
+					<p>These Terms shall be governed and construed in accordance with the laws of Hungary, without regard to its conflict of law provisions.</p>
+
+					<p>Our failure to enforce any right or provision of these Terms will not be considered a waiver of those rights. If any provision of these Terms is held to be invalid or unenforceable by a court, the remaining provisions of these Terms will remain in effect. These Terms constitute the entire agreement between us regarding our Service, and supersede and replace any prior agreements we might have between us regarding the Service.</p>
+
+					<h3>Changes</h3>
+
+					<p>We reserve the right, at our sole discretion, to modify or replace these Terms at any time. If a revision is material we will try to provide at least 30 days notice prior to any new terms taking effect. What constitutes a material change will be determined at our sole discretion.</p>
+
+					<p>By continuing to access or use our Service after those revisions become effective, you agree to be bound by the revised terms. If you do not agree to the new terms, please stop using the Service.</p>
+
+					<h3>Contact Us</h3>
+
+					<p>If you have any questions about these Terms, please contact us.</p>
+				</section>
+			</div>
+		</Layout>
+	);
+}
