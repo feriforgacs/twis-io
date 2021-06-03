dd7de9f702885f86dc5ea6a727c4ada1440e12b8
Ferenc Forgacs <forgacsf@gmail.com>
2021-06-03 21:29:56 +0200

    layout fix on smaller screens

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,16 @@
 export default function Footer() {
 	return (
-		<footer id="footer">
-			<div className="container">Footer</div>
+		<footer className="footer">
+			<div className="container">
+				<div className="footer__contact">
+					<h4>Contact and Support</h4>
+					<p>
+						If you have any questions feel free to get in touch via email at <a href="mailto:support@twis.io">support@twis.io</a>.
+					</p>
+				</div>
+
+				<nav className="footer__nav"></nav>
+			</div>
 		</footer>
 	);
 }
