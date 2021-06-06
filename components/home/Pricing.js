3ed27dd9d61d3a2bcc7d43b16c97d65a8f37361b
Ferenc Forgacs <forgacsf@gmail.com>
2021-06-06 08:39:39 +0200

    [wip] pricing

--- components/home/Pricing.js
+++ components/home/Pricing.js
@@ -1,12 +1,15 @@
+import { useState } from "react";
+
 export default function Pricing() {
+	const [term, setTerm] = useState("yearly");
 	return (
 		<div id="pricing">
 			<h3>Pricing</h3>
-			<div id="pricing__terms">
-				<button>
+			<div id="pricing__terms" className={`term--${term}`}>
+				<button onClick={() => setTerm("yearly")}>
 					Billed yearly <span>-20%</span>
 				</button>
-				<button>Billed monthly</button>
+				<button onClick={() => setTerm("monthly")}>Billed monthly</button>
 			</div>
 		</div>
 	);
