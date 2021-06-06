fea43dd07346a72891376d7010231f0b7ddce0d2
Ferenc Forgacs <forgacsf@gmail.com>
2021-06-06 08:55:17 +0200

    [wip] pricing

--- components/home/Pricing.js
+++ components/home/Pricing.js
@@ -1,7 +1,35 @@
 import { useState } from "react";
+import PricingOption from "./PricingOption";
 
 export default function Pricing() {
 	const [term, setTerm] = useState("yearly");
+
+	const pricingOptions = [
+		{
+			basic: {
+				name: "👍 Basic",
+				priceMonthly: "$29",
+				priceYearly: "$23",
+				participantLimit: 100,
+				overagesCost: "$0.25",
+			},
+			pro: {
+				name: "⭐ Pro",
+				priceMonthly: "$49",
+				priceYearly: "$39",
+				participantLimit: 1000,
+				overagesCost: "$0.05",
+			},
+			premium: {
+				name: "🚀 Premium",
+				priceMonthly: "$69",
+				priceYearly: "$55",
+				participantLimit: 10000,
+				overagesCost: "$0.008",
+			},
+		},
+	];
+
 	return (
 		<div id="pricing">
 			<h3>Pricing</h3>
@@ -10,6 +38,10 @@ export default function Pricing() {
 					Billed yearly <span>-20%</span>
 				</button>
 				<button onClick={() => setTerm("monthly")}>Billed monthly</button>
+			</div>
+
+			<div id="pricing__options">
+				<PricingOption term={term} name="👍 Basic" price="$29" participantLimit="100" overagesCost="$0.25" />
 			</div>
 		</div>
 	);
