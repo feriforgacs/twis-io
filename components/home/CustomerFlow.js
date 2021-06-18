47ed978f9f82435fdd704b4102d545e5af601bb7
Ferenc Forgacs <forgacsf@gmail.com>
2021-06-18 17:44:31 +0200

    [wip] engagement block

--- components/home/CustomerFlow.js
+++ components/home/CustomerFlow.js
@@ -3,11 +3,28 @@ import Image from "next/image";
 export default function CustomerFlow() {
 	return (
 		<div className="full-width full-width--customer-flow">
+			<h3>Build awareness in a fun and engaging way</h3>
 			<div className="container container--customer-flow">
 				<div className="customer-flow__col">
 					<h4>Engage</h4>
 					<div className="customer-flow__illustration customer-flow__illustration--engage">
-						<div className="screen"></div>
+						<div className="screen">
+							<div className="emoji">
+								<Image src="/images/home/emoji--heart-eyes.png" width={30} height={30} />
+							</div>
+
+							<div className="emoji">
+								<Image src="/images/home/emoji--joy.png" width={30} height={30} />
+							</div>
+
+							<div className="emoji">
+								<Image src="/images/home/emoji--sparkling-heart.png" width={30} height={30} />
+							</div>
+
+							<div className="emoji">
+								<Image src="/images/home/emoji--clapping-hands.png" width={30} height={30} />
+							</div>
+						</div>
 					</div>
 					<p>todo</p>
 				</div>
@@ -34,14 +51,6 @@ export default function CustomerFlow() {
 							<div className="input-field grow-screen-item">your@mail.com</div>
 							<div className="submit-button grow-screen-item">submit</div>
 						</div>
-					</div>
-					<p>todo</p>
-				</div>
-
-				<div className="customer-flow__col">
-					<h4>Build awereness</h4>
-					<div className="customer-flow__illustration customer-flow__illustration--build">
-						<div className="screen"></div>
 					</div>
 					<p>todo</p>
 				</div>
