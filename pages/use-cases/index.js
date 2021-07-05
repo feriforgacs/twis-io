688145abd4fbe13900e36c4df765de57821158f5
Ferenc Forgacs <forgacsf@gmail.com>
2021-07-05 21:19:01 +0200

    [wip] use cases page

--- pages/use-cases/index.js
+++ pages/use-cases/index.js
@@ -10,6 +10,48 @@ export default function UseCases() {
 							<h1 className="align--center">Use cases</h1>
 							<h2 className="align--center font--mulish">What twis can do for you?</h2>
 						</div>
+
+						<div className="use-cases">
+							<div className="use-case-item">
+								<div className="use-case-item__description">
+									<h4>From TikTok videos to twis campaigns</h4>
+									<p>###TODO tiktok video » not ideal to grow db » drive visitors to twis campaign » ask them to play a short game » give them a small gift » collect their names and email addresses » select random winners</p>
+								</div>
+
+								<div className="use-case-item__illustration">illustration</div>
+							</div>
+							{/* end .use-case-item */}
+
+							<div className="use-case-item">
+								<div className="use-case-item__description">
+									<h4>Grow your database with the combination of Instagram story ads and twis campaigns</h4>
+									<p>###TODO sometimes a story is too short to convince someone about something » use a twis campaign to highlight the benefints of your product » do it by asking educative questions » give them a reward, eg a discount code they can use on their first order</p>
+								</div>
+
+								<div className="use-case-item__illustration">illustration</div>
+							</div>
+							{/* end .use-case-item */}
+
+							<div className="use-case-item">
+								<div className="use-case-item__description">
+									<h4>Make sure viewers pay attention to your YouTube videos</h4>
+									<p>###TODO set up a quick twis quiz with a few questions about your latest youtube video » draw random winners who had at least 3 correct answers</p>
+								</div>
+
+								<div className="use-case-item__illustration">illustration</div>
+							</div>
+							{/* end .use-case-item */}
+
+							<div className="use-case-item">
+								<div className="use-case-item__description">
+									<h4>Teach something new and give an instant reward</h4>
+									<p>###TODO Coinbase like sample</p>
+								</div>
+
+								<div className="use-case-item__illustration">illustration</div>
+							</div>
+							{/* end .use-case-item */}
+						</div>
 					</div>
 				</div>
 			</div>
