1b23dd6191302247b098434d2ec70ac3bdd2cda6
Ferenc Forgacs <forgacsf@gmail.com>
2021-08-16 17:49:02 +0200

    copy changes

--- pages/pricing.js
+++ pages/pricing.js
@@ -13,11 +13,11 @@ export default function PricingPage() {
 
 						<Pricing showTitle={false} lightVersion={true} />
 
-						<h3>What's in the free plan?</h3>
-						<p>When you create a twis account you'll have access to all the features and you can collect 10 participants for free. This is enough to take a look at the capabilities of the platform and you can also run a test campaign. If you collect more than 10 participants during the trial period, you won't be charged only if you chose to go with one of our paid plans.</p>
+						<h3>What can you try for free?</h3>
+						<p>When you create a twis account you'll have access to all the features and you can collect 10 participants for free. This is enough to take a look at the capabilities of the platform and you can also run a tiny test campaign. If you collect more than 10 participants during the trial period, you won't be charged only if you chose to go with one of our paid plans.</p>
 
 						<p>
-							<strong>You can cancel your subscription any time.</strong>
+							<strong>You can cancel your subscription at any time.</strong>
 						</p>
 					</div>
 				</div>
